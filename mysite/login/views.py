b7b840cc792db5277344c2963235d5c7566c6038
Yasin_win10 <yasinliu615@gmail.com>
2021-02-03 01:21:33 +0800

    更新至动态页面

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 
+user_list = []
+
 def index(request):
     # return HttpResponse('Hello 白鬓少年')
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        temp = {'user': username, 'pwd': password}
+        user_list.append(temp)
+    return render(request, 'index.html', {'data': user_list})
