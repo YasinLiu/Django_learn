3b4e7700575eeb3445795d0db996a5608f367c3e
Yasin_jjj_pc <yasinliu615@gmail.com>
2021-02-03 16:12:56 +0800

    finish the first project login

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from login import models
 
 # Create your views here.
 
-user_list = []
 
 def index(request):
+    # return HttpResponse('Hello 白鬓少年')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # 将数据保存到数据库
+        models.UserInfo.objects.create(user=username, pwd=password)
+
+    # 从数据库中读取所有数据，注意缩进
+    user_list = models.UserInfo.objects.all()
+    return render(request, 'index.html', {'data': user_list})
+
+
+# user_list = []
+def index_back(request):
     # return HttpResponse('Hello 白鬓少年')
     if request.method == 'POST':
         username = request.POST.get('username')
