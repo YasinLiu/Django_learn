d835dbe87acdb37dc8ca15e2079669b63fd690b5
Yasin pc_home <yasinliu615@gmail.com>
2022-03-30 01:42:00 +0800

    sycn 4

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,15 @@
+from asyncio import futures
 from django.test import TestCase
 
+import datetime
+from django.utils import timezone
+from .models import Question
+
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+	def test_was_publishec_recently_with_future_question(self):
+		"""test_was_publishec_recently_with_future_question 在将来发布的问卷应该返回False
+		"""		
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertIs(future_question.was_published_recently(), False)
