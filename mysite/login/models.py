09ff5b8b39912e2e4c676b2fbf9856d078fb0da4
YasinLiu <yasinliu615@gmail.com>
2022-04-21 17:43:08 +0800

    注册视图

--- mysite/login/models.py
+++ mysite/login/models.py
@@ -1,15 +1,15 @@
 import email
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
-    
     gender = (
         ('male', '男'),
         ('female', '女'),
     )
-    
+
     name = models.CharField(max_length=128, unique=True)
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
@@ -21,7 +21,7 @@ class User(models.Model):
 
     # def __str__(self) -> str:
     #     return super().__str__()
-    
+
     class Meta:
         ordering = ['-c_time']
         verbose_name = '用户'
