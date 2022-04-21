d0932e19dcbce35ce1d4919ae2183e71167c7c30
YasinLiu <yasinliu615@gmail.com>
2022-04-21 16:19:57 +0800

    登录、表单、图验、session

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render, redirect
-from login import models
-
-
+from . import forms, models
 # Create your views here.
 
+
+def index(request):
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    if request.session.get('is_login', None): #不允许重复登录
+        return redirect('/index/')
+    if request.method == "POST":
+        login_form = forms.UserForm(request.POST)
+        message = '请检查填写的内容！'
+        # 前端验证
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                message = '用户不存在!'
+                return render(request, 'login/login.html', locals())
+
+            if user.password == password:
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
+                return redirect('/index/')
+            else:
+                message = '密码不正确'
+                return render(request, 'login/login.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
+
+
+def register(request):
+    pass
+    return render(request, 'login/register.html')
+
+
+def logout(request):
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出这一说
+        return redirect('/login/')
+    request.session.flush()
+    return redirect('/login/')
 
 # def index(request):
 #     # return HttpResponse('Hello 白鬓少年')
@@ -28,22 +74,3 @@ from login import models
 #         temp = {'user': username, 'pwd': password}
 #         user_list.append(temp)
 #     return render(request, 'index.html', {'data': user_list})
-
-def index(request):
-    pass
-    return render(request, 'login/index.html')
-
-
-def login(request):
-    pass
-    return render(request, 'login/login.html')
-
-
-def register(request):
-    pass
-    return render(request, 'login/register.html')
-
-
-def logout(request):
-    pass
-    return redirect('/login/')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'login'
+    'login',
+    'captcha',
 ]
 
 MIDDLEWARE = [
